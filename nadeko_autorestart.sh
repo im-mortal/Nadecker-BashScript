f4ba85995ee9a4c87ee7e269c3beff50d3126fee
sh1ny <sh1ny@me.com>
2018-07-23 09:17:55 +0300

    Merge with master, add --no-build to run command

--- nadeko_autorestart.sh
+++ nadeko_autorestart.sh
@@ -1,32 +1,35 @@
-#!/bin/sh
+#!/bin/bash
+info() {
+    printf '%s\n' "$@"
+}
 
-if hash dotnet 2>/dev/null
-then
-	echo "Dotnet installed."
+if hash dotnet 2>/dev/null ; then
+	info "Dotnet installed."
 else
-	echo "Dotnet is not installed. Please install dotnet."
+	info "Dotnet is not installed. Please install dotnet."
 	exit 1
 fi
 
-echo ""
-echo "Linking Nadeko Credentials"
+info '' "Linking Nadeko Credentials"
 mv -n /opt/NadekoBot/src/NadekoBot/credentials.json /root/nadeko/credentials.json > /dev/null 2>&1
 rm /opt/NadekoBot/src/NadekoBot/credentials.json > /dev/null 2>&1
 ln -s /root/nadeko/credentials.json /opt/NadekoBot/src/NadekoBot/credentials.json > /dev/null 2>&1
 
-echo ""
-echo "Patching Nadeko Data Folder"
-mkdir -p /root/nadeko/patch
-cd /root/nadeko/patch && bash "./patch.sh"
+patchfile="/root/nadeko/patch/patch.sh"
+mkdir -p $(dirname $patchfile)
+if [ -s "$patchfile" ] && [ -x "$patchfile" ]; then
+	info '' "Patching Nadeko Data Folder"
+	bash "/root/nadeko/patch/patch.sh"
+else
+	info '' "Patch file does not exist"
+fi
 
-echo ""
-echo "Starting Redis-Server"
+info '' "Starting Redis-Server"
 /usr/bin/redis-server --daemonize yes
 
-echo ""
-echo "Running NadekoBot with auto restart Please wait."
+info '' "Running NadekoBot with auto restart" "Please wait"
 cd /opt/NadekoBot/src/NadekoBot
-while :; do dotnet run -c Release; sleep 5s; done
-echo "Done"
+while :; do dotnet run -c Release --no-build; sleep 5s; done
+info "Done"
 
 exit 0
